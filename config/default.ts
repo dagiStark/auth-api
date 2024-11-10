9361cd592f6c76ccd5a9308db2d334aed5574174
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-10 23:15:28 +0300

    setup logger

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,5 @@ export default {
   port: 3000,
   dbUri:
     "mongodb+srv://dagigelaneh26:jdMSpgXulOSQf4bR@cluster0.5rpnz9o.mongodb.net/auth_api?retryWrites=true&w=majority&appName=Cluster0",
+  logLevel: "info",
 };
