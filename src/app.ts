9ff9b663d4518e3d3b9111e00ef433f7451506ba
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-10 21:10:20 +0300

    database connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 require("dotenv");
 import express from "express";
 import config from "config";
+import connectToDb from "./utils/connectToDb";
 
 const app = express();
 
 const port = config.get("port");
 
 app.listen(port, () => {
+  connectToDb();
   console.log(`app started listening on port ${port}`);
 });
