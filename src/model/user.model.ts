b8ff3b862ed9045bdc1fd33c6ecbd141560377a2
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-11 09:23:10 +0300

    create user db model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,6 +1,59 @@
-import { prop } from "@typegoose/typegoose";
+import {
+  prop,
+  getModelForClass,
+  modelOptions,
+  Severity,
+  pre,
+  DocumentType,
+} from "@typegoose/typegoose";
+import * as argon2d from "argon2";
+import { nanoid } from "nanoid";
 
+@modelOptions({
+  schemaOptions: { timestamps: true },
+  options: { allowMixed: Severity.ALLOW },
+})
+@pre<User>("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  const hash = await argon2d.hash(this.password);
+  this.password = hash;
+  return;
+})
 export class User {
   @prop({ lowercase: true, required: true, unique: true })
   email: string;
+
+  @prop({ required: true })
+  firstName: string;
+
+  @prop({ required: true })
+  lastName: string;
+
+  @prop({ required: true })
+  password: string;
+
+  @prop({ required: true, default: () => nanoid() })
+  verificationCode: string;
+
+  @prop()
+  passwordResetCode: string | null;
+
+  @prop({ default: false })
+  verified: boolean;
+
+  async validatePassword(this: DocumentType<User>, candidatePassword: string) {
+    try {
+      return await argon2d.verify(this.password, candidatePassword);
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  
 }
+
+const UserModel = getModelForClass(User);
+export default UserModel;
